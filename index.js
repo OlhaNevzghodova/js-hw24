c52ca5ed7d8e06444e6195328f3bfd801b723031
olha nevzghodova <onevz.alex@gmail.com>
2023-03-21 17:13:47 +0200

    hw24 is fixed

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-const images = ['rat1.jpg', 'rat2.jpg', 'rat3.jpg', 'rat4.jpg', 'rat5.jpg', 'rat6.jpg', 'rat7.jpg', 'rat8.jpg', 'rat9.jpg'];
-
-let count = images.length;
-let random = Math.floor(Math.random() * count);
-document.write('<img src="./images/' + images[random] + '" alt="image"/>');
+const images = document.querySelector('.images');
+images.innerHTML = `<img src="./images/rat${Math.floor(Math.random() * (9 - 1) + 1)}.jpg" alt="image"/>`
